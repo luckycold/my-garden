5c5987672372ba26419c4b3d0f96b1ace6ab3556
Luke D Williams <github@luckycold.me>
2023-09-20 15:47:28 -0500

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -5,5 +5,10 @@ const config = {
   },
   showEditLink: true,
   editLinkRoot: "https://github.com/luckycold/my-blog/edit/main",
+  navbarTitle: {
+    logo: "/images/logo.svg",
+    text: "Luke's Mind Garden",
+    version: "Alpha",
+  },
 }
 export default config;
