887e26206a0f9964dc51283eaa557c584b6d4bb8
Luke D Williams <github@luckycold.me>
2023-09-20 15:23:29 -0500

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -2,6 +2,8 @@ const config = {
   defaultAuthor: "Luke Williams",
   search: {
     provider: "kbar"
-  }
+  },
+  showEditLink: true,
+  editLinkRoot: "https://github.com/luckycold/my-blog/edit/main/site",
 }
 export default config;
